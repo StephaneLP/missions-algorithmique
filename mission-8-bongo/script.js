f4a0d438116d3c2e5e84ed4eacc4b39716697c92
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-03-23 19:23:00 +0100

    maj 23/03/23

--- mission-8-bongo/script.js
+++ mission-8-bongo/script.js
@@ -1,42 +1,49 @@
-// Tableau contenant les animaux et variable recevant le résultat du lancé de tous les dés
+// Tableau contenant les animaux et les variables recevant le résultat du lancé des dés
 
 const animaux = ["Antilope","Gnou","Rhinocéros"]
-const lanceDeDes = {
-    désJaunes: [0,0],
-    désBlancs: ["","","","",""],
-    désRouges: ["",""],
-    déVert: ""
+const desJaunes = [0,0]
+const desAnimaux = {
+    blancs: ["","","","",""],
+    rouges: ["",""],
+    vert: ""
 }
 
-// Initialisation de la variable lanceDes
+// Initialisation des dés jaunes et calcul du nombre d'animaux (d'une espèce) à chercher
 
-for(let i=0; i<2; i++) {
-    lanceDeDes.désJaunes[i] = getRandomIntInclusive(1,3)
+for(let i=0; i<2; i++) desJaunes[i] = getRandomIntInclusive(1,3)
+let nbAnimauxAChercher = (desJaunes[0]===desJaunes[1] ? desJaunes[0] : 6-(desJaunes[0]+desJaunes[1]))
+
+// Initialisation des dés animaux
+
+for(let couleur in desAnimaux) {
+console.log(desAnimaux[couleur])
+    // for(let i in desAnimaux[couleur]){
+    //     console.log(i)
+    //     desAnimaux.couleur[i] = animaux[getRandomIntInclusive(0,2)]
+    //     console.log(desAnimaux.couleur[i])
+    // }
 }
-for(let i=0; i<5; i++) {
-    lanceDeDes.désBlancs[i] = animaux[getRandomIntInclusive(0,2)]
-}
-for(let i=0; i<2; i++) {
-    lanceDeDes.désRouges[i] = animaux[getRandomIntInclusive(0,2)]
-}
-lanceDeDes.déVert = animaux[getRandomIntInclusive(0,2)]
+
+
+
+// for(let i=0; i<5; i++) {
+//     lanceDeDes.désBlancs[i] = animaux[getRandomIntInclusive(0,2)]
+// }
+// for(let i=0; i<2; i++) {
+//     lanceDeDes.désRouges[i] = animaux[getRandomIntInclusive(0,2)]
+// }
+// lanceDeDes.déVert = animaux[getRandomIntInclusive(0,2)]
 
 // Calcul du nombre d'animaux à chercher
 
-let nbAnimauxAChercher
-if(lanceDeDes.désJaunes[0]===lanceDeDes.désJaunes[1]) {
-    nbAnimauxAChercher = lanceDeDes.désJaunes[0]
-}
-else {
-    nbAnimauxAChercher = 6-(lanceDeDes.désJaunes[0]+lanceDeDes.désJaunes[1])
-}
+
 
 // Affichage  du résultat du lancé de dés :
 
-console.log("Balles : ", afficheTableau(lanceDeDes.désJaunes), "(Nombre d'animaux à chercher : ", nbAnimauxAChercher,")")
-console.log("Animaux : ", afficheTableau(lanceDeDes.désBlancs))
-console.log("Braconniers : ", afficheTableau(lanceDeDes.désRouges))
-console.log("Garde chasse : ", lanceDeDes.déVert)
+console.log("Balles : ", afficheTableau(desJaunes), "(Nombre d'animaux à chercher : ", nbAnimauxAChercher,")")
+console.log("Animaux : ", afficheTableau(desAnimaux.blancs))
+console.log("Braconniers : ", afficheTableau(desAnimaux.rouges))
+console.log("Garde chasse : ", desAnimaux.vert)
 
 // Outils
 
